02f5019b14dfbe4f0b20bbd615e90657e5464a4a
Samuel22-glitch <samuelajudua22@gmail.com>
2025-06-16 23:35:19 +0100

    Add files via upload

---
+++ currentaccount.py
@@ -0,0 +1,17 @@
+from account import Account
+
+class currentAccount(Account):
+    def __init__(self, balance,):
+        Account.__init__(self, balance)
+
+    def withdraw(self, amount):
+        if amount < 0:
+            raise ValueError("Amount cannot be negative")
+        if amount > self.balance:
+            raise ValueError("Insufficient Funds")
+        self.balance -= amount
+
+    def deposit(self, amount):
+
+       if amount >= 0:
+           self.balance += amount
---
+++ home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(page_title="Bank App", layout="centered")
+
+st.title("X-PAY")
+st.title("Banking Made Simple, Secure, and Smart")
+st.subheader("Your financial future starts here")
+st.write("Manage your money with confidence—anytime, anywhere.")
+st.write("* Enjoy 24/7 secure online banking")
+st.write("* Get expert financial guidance")
